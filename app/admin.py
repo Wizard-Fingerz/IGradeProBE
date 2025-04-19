d7b7ed5ef62aa4a0ae337702157c9f4fb65c16ad
WizCode <adewale.oladiti28@gmail.com>
2025-04-19 11:14:51 +0100

    `Added admin registration for Exam and SubjectQuestion models, updated serializers for exams and questions to handle nested relationships, and modified the CreateExamSerializer to recreate questions and sub-questions when updating an exam.`

--- app/admin.py
+++ app/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 
 from import_export.admin import ImportExportModelAdmin
 
+from app.exams.models import Exam
+from app.questions.models import SubjectQuestion
 from app.results.models import ExamResult
 from app.subjects.models import Subject
 
@@ -20,6 +22,19 @@ class ExamResultAdmin(ImportExportModelAdmin):
     list_display = ('student','question','student_answer', 'student_score', 'similarity_score', 'attempted')
     search_fields = ['student',]
     list_filter = ['student',]
+
+@admin.register(Exam)
+class ExamAdmin(ImportExportModelAdmin):
+    list_display = ('question_count','duration','instruction', 'subject', 'total_mark', 'exam_type', 'exam_year', 'question_count', 'subject_name', 'created_by_name',
+                    )
+    search_fields = ['subject',]
+    list_filter = ['subject',]
+
+@admin.register(SubjectQuestion)
+class SubjectQuestionAdmin(ImportExportModelAdmin):
+    list_display = ('subject', 'question_number', 'comprehension', 'question', 'examiner_answer', 'question_score', 'is_optional', 'parent_question')
+    search_fields = ['subject', 'question_number']
+    list_filter = ['subject', 'question_number']
 
 
 admin.site.site_header = 'Intelligent Essay Grading Pro Administration Dashboard'
--- app/exams/serializers.py
+++ app/exams/serializers.py
@@ -20,19 +20,17 @@ class SubSubQuestionSerializer(serializers.ModelSerializer):
         model = SubjectQuestion
         fields = ['id', 'question_number', 'comprehension', 'question',
                   'examiner_answer', 'question_score', 'is_optional', 'parent_question']
-        ref_name = 'ExamSubSubQuestionSerializer'  # Add ref_name to avoid conflict
-
+        ref_name = 'ExamSubSubQuestionSerializer'
 
 class SubQuestionSerializer(serializers.ModelSerializer):
     """Serializer for sub-questions, including sub-sub-questions."""
-    sub_questions = SubSubQuestionSerializer(many=True, required=False)
+    sub_sub_questions = SubSubQuestionSerializer(many=True, required=False)
 
     class Meta:
         model = SubjectQuestion
         fields = ['id', 'question_number', 'comprehension', 'question',
-                  'examiner_answer', 'question_score', 'is_optional', 'parent_question', 'sub_questions']
-        ref_name = 'ExamSubQuestionSerializer'  # Add ref_name to avoid conflict
-
+                  'examiner_answer', 'question_score', 'is_optional', 'parent_question', 'sub_sub_questions']
+        ref_name = 'ExamSubQuestionSerializer'
 
 class SubjectQuestionNestedSerializer(serializers.ModelSerializer):
     """Serializer for main questions, including sub-questions."""
@@ -40,7 +38,7 @@ class SubjectQuestionNestedSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = SubjectQuestion
-        fields = ['question_number', 'comprehension', 'question',
+        fields = ['id', 'question_number', 'comprehension', 'question',
                   'examiner_answer', 'question_score', 'is_optional', 'sub_questions']
         ref_name = 'QuestionSubQuestionSerializer'
 
@@ -50,7 +48,8 @@ class SubjectQuestionNestedSerializer(serializers.ModelSerializer):
         subject_question = SubjectQuestion.objects.create(**validated_data)
 
         for sub_question_data in sub_questions_data:
-            sub_sub_questions_data = sub_question_data.pop('sub_questions', [])
+            sub_sub_questions_data = sub_question_data.pop('sub_sub_questions', [])
+            print(sub_sub_questions_data)
             sub_question_data['subject'] = subject
             sub_question_data['parent_question'] = subject_question
             sub_question_instance = SubjectQuestion.objects.create(**sub_question_data)
@@ -62,7 +61,7 @@ class SubjectQuestionNestedSerializer(serializers.ModelSerializer):
                 SubjectQuestion.objects.create(**sub_sub_question_data)
 
         return subject_question
-
+    
 
 class CreateExamSerializer(serializers.ModelSerializer):
     """Serializer for creating exams, including nested questions."""
@@ -87,52 +86,20 @@ class CreateExamSerializer(serializers.ModelSerializer):
     
     def update(self, instance, validated_data):
         questions_data = validated_data.pop('questions', [])
-        instance.exam_year = validated_data.get('exam_year', instance.exam_year)
-        instance.exam_type = validated_data.get('exam_type', instance.exam_type)
-        instance.total_mark = validated_data.get('total_mark', instance.total_mark)
-        instance.paper_number = validated_data.get('paper_number', instance.paper_number)
-        instance.subject = validated_data.get('subject', instance.subject)
+
+        # Update exam fields
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
         instance.save()
 
-        # Track existing question IDs to avoid recreating them
-        existing_question_ids = [question.id for question in instance.questions.all()]
+        # Delete existing questions (cascade should handle sub-questions and sub-sub)
+        instance.questions.all().delete()
 
+        # Recreate questions
         for question_data in questions_data:
-            question_id = question_data.get('id', None)
-            if question_id and question_id in existing_question_ids:
-                # Update existing question
-                question_instance = SubjectQuestion.objects.get(id=question_id)
-                for attr, value in question_data.items():
-                    if attr == 'sub_questions':  # Handle sub-questions separately
-                        sub_questions_data = value
-                        existing_sub_question_ids = [sub.id for sub in question_instance.sub_questions.all()]
-                        for sub_question_data in sub_questions_data:
-                            sub_question_id = sub_question_data.get('id', None)
-                            if sub_question_id and sub_question_id in existing_sub_question_ids:
-                                # Update existing sub-question
-                                sub_question_instance = SubjectQuestion.objects.get(id=sub_question_id)
-                                for sub_attr, sub_value in sub_question_data.items():
-                                    setattr(sub_question_instance, sub_attr, sub_value)
-                                sub_question_instance.save()
-                            else:
-                                # Create new sub-question
-                                sub_question_data['parent_question'] = question_instance
-                                SubjectQuestion.objects.create(**sub_question_data)
-                    else:
-                        setattr(question_instance, attr, value)
-                question_instance.save()
-            else:
-                # Create new question using the create method
-                sub_questions_data = question_data.pop('sub_questions', [])
-                question_data['subject'] = instance.subject
-                question_instance = SubjectQuestion.objects.create(**question_data)
-
-                # Handle sub-questions for the new question
-                for sub_question_data in sub_questions_data:
-                    sub_question_data['parent_question'] = question_instance
-                    SubjectQuestion.objects.create(**sub_question_data)
-
-                instance.questions.add(question_instance)
+            question_data['subject'] = instance.subject
+            question_instance = SubjectQuestionNestedSerializer().create(question_data)
+            instance.questions.add(question_instance)
 
         return instance
 
