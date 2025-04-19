75fce23c05de175043534dfb07c7a7bf4133486e
WizCode <adewale.oladiti28@gmail.com>
2025-04-19 08:35:40 +0100

    `Added update method to CreateExamSerializer to handle updating exams and their nested questions.`

--- app/exams/serializers.py
+++ app/exams/serializers.py
@@ -83,7 +83,53 @@ class CreateExamSerializer(serializers.ModelSerializer):
             exam.questions.add(question_instance)
 
         return exam
+    def update(self, instance, validated_data):
+        questions_data = validated_data.pop('questions', [])
+        instance.exam_year = validated_data.get('exam_year', instance.exam_year)
+        instance.exam_type = validated_data.get('exam_type', instance.exam_type)
+        instance.total_mark = validated_data.get('total_mark', instance.total_mark)
+        instance.paper_number = validated_data.get('paper_number', instance.paper_number)
+        instance.subject = validated_data.get('subject', instance.subject)
+        instance.save()
 
+        # Update or create nested questions
+        for question_data in questions_data:
+            question_id = question_data.get('id', None)
+            if question_id:
+                # Update existing question
+                question_instance = SubjectQuestion.objects.get(id=question_id)
+                for attr, value in question_data.items():
+                    if attr == 'sub_questions':  # Handle sub-questions separately
+                        sub_questions_data = value
+                        for sub_question_data in sub_questions_data:
+                            sub_question_id = sub_question_data.get('id', None)
+                            if sub_question_id:
+                                # Update existing sub-question
+                                sub_question_instance = SubjectQuestion.objects.get(id=sub_question_id)
+                                for sub_attr, sub_value in sub_question_data.items():
+                                    setattr(sub_question_instance, sub_attr, sub_value)
+                                sub_question_instance.save()
+                            else:
+                                # Create new sub-question
+                                sub_question_data['parent_question'] = question_instance
+                                SubjectQuestion.objects.create(**sub_question_data)
+                    else:
+                        setattr(question_instance, attr, value)
+                question_instance.save()
+            else:
+                # Create new question
+                sub_questions_data = question_data.pop('sub_questions', [])
+                question_data['subject'] = instance.subject
+                question_instance = SubjectQuestion.objects.create(**question_data)
+
+                # Handle sub-questions for the new question
+                for sub_question_data in sub_questions_data:
+                    sub_question_data['parent_question'] = question_instance
+                    SubjectQuestion.objects.create(**sub_question_data)
+
+                instance.questions.add(question_instance)
+
+        return instance
 
 class GetExamDetailSerializer(serializers.ModelSerializer):
     """Serializer for retrieving exam details, including nested questions."""
